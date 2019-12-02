f264a9f59b581301dddc68c7b12a9bf1a0926d47
anziferovma <anziferovma@s5gub.ru>
2019-12-02 23:15:43 +0300

    add labs 1.1 1.2 1.3

---
+++ lab_1/lab_A/FormationMatrix.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class FormationMatrix {
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+
+    System.out.println("Enter matrix i, then j:");
+    int[][] matrix = makeArray(scn.nextInt(), scn.nextInt());
+    for (int[] line : matrix) {
+      for (int num : line) {
+        System.out.print(num + " ");
+      }
+      System.out.println();
+    }
+    scn.close();
+  }
+
+  public static int[][] makeArray(int i, int j) {
+    int[][] arr = new int[i][j];
+    for (int a = 0; a < i; a++) {
+      arr[n][0] = 1;
+      for (int b = 1; b < j; b++) {
+        arr[a][b] = 0;
+      }
+    }
+    return arr;
+  }
+}
---
+++ lab_1/lab_B/StringСonversion.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class StringСonversion {
+  static String message = "none";
+  static Scanner scan = new Scanner(System.in);
+
+  public static void main(String[] args) {
+
+    while (message.length() % 3 != 0) {
+     System.out.println(String length must be divided by 3 without reminder");
+      message = scan.next();
+    }
+
+    String[] newArray = separation(message);
+    for (int i = 0; i < newArray.length; i++) {
+      newArray[i] = randMidChar(newArray[i]);
+    }
+    Arrays.sort(newArray);
+
+    for (String x : newArray) {
+      System.out.println(x);
+    }
+    scan.close();
+  }
+
+  public static String[] separation(String message) {
+    return message.split("(?<=\\G...)");
+  }
+
+  public static String randMidChar(String message) {
+    StringBuilder newMessage = new StringBuilder(message);
+    Random rnd = new Random();
+    char newChar = '0';
+    while (newChar == newMessage.charAt(0) || newChar == newMessage.charAt(1) 
+      || newChar == newMessage.charAt(2) || newChar == '0') {
+      newChar = (char) (rnd.nextInt(26) + 'a');
+    }
+    newMessage.setCharAt(1, newChar);
+    return newMessage.toString();
+  }
+}
